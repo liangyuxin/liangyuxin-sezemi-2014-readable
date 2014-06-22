f7955565751a99b7d1d2273113ee5291fd8960fa
liangyuxin <liangyuxin278@gmail.com>
2014-06-22 12:03:49 +0900

    commit

--- readable_code/src/first.java
+++ readable_code/src/first.java
@@ -1,6 +1,26 @@
+import java.io.*;
+
+
 public class first {
 	public static void main(String args[]) {
-		System.out.println("オムライス");
-	}
+		//System.out.println("オムライス");
+
+		// read omu-rice from file
+		try{
+		      File file = new File("recipe-data.txt");
+		      FileReader filereader = new FileReader(file);
+
+		      int ch;
+		      while((ch = filereader.read()) != -1){
+		        System.out.print((char)ch);
+		      }
+
+		      filereader.close();
+		    }catch(FileNotFoundException e){
+		      System.out.println(e);
+		    }catch(IOException e){
+		      System.out.println(e);
+		    }
+		  }
 }
 
